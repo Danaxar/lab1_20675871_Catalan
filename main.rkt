f47fc2327ebb8ce5ff2574044db69d57e9f1d2fa
Daniel Catalán <daniel.catalan@usach.cl>
2021-10-16 02:45:20 -0400

    Primer avance

---
+++ main.rkt
@@ -0,0 +1,96 @@
+#lang racket
+
+"Bienvenido a paradigmasdocs"
+"¿Qué desea hacer?"
+"1) Iniciar sesión"
+"2) Registrarse"
+
+"Escriba <(inicio __su_respuesta__)> para comenzar con la ejecución"
+
+(define inicio
+	(lambda(primeraRespuesta)
+		; Aqui se decide si inicia sesión o se registra
+		(if (= primeraRespuesta 1)
+			"Ha escogido iniciar sesión"
+			; Caso verdadero (iniciar sesión)
+				; Pedir usuario y contraseña
+				; Verificar que exista en algún registro
+			"Ha escogido registrarse"
+			"Por favor ingrese su nombre de nuevo usuario"
+			; Caso falso (Registrarse)
+				; Pedir usuario
+				; Pedir contraseña
+				; Pedir que confirme la contraseña
+				; Escribir usuario y contraseña en registro
+		)
+	)
+)
+
+
+"Funciones disponibles"
+"1) Crear un documento"
+"2) Eliminar un documento"
+"3) Compartir un documento"
+"4) Asignar permisos sobre un documento"
+"5) Editar un documento"
+
+(define accion
+	(lambda x
+		(if (= x 1)
+			; Caso verdadero
+				; Crear un documento
+			; Caso falso -> otras opciones
+			(if (= x 2)
+				; Caso verdadero
+					; Eliminar un documento
+				; Caso falso
+				(if (= x 3)
+					; Caso verdadero
+						; Compartir un documento
+					; Caso falso
+					(if (= x 4)
+						; Caso verdadero
+							; Asignar permisos sobre un documento
+						; Caso falso
+						(if (= x 5)
+							; Caso verdadero
+							"Que acción desea hacer?"
+							"a) Escribir"
+							"b) Cambiar estilos a los textos (ej: cursiva, negrita, etc.)"
+							"c) Buscar y reemplazar"
+							"d) Copiar texto"
+							"e) Pegar texto"
+							"f) Cortar texto"
+							"g) Crear títulos, subtítulos"
+							"h) Crear un índice"
+							(lambda(y)
+								(if (= y a)
+									; v
+									(if (= y b)
+										() ; v
+										(if (= y c)
+											() ; v
+											(if (= y d)
+												() ; v
+												(if (= y e)
+													() ; v
+													(if (= y f)
+														()
+														(if (= y g)
+															()
+															() ; Opción h
+														)
+													)
+												)
+											)
+										)
+									)
+								)
+							)
+						)
+					)
+				)
+			)
+		)
+	)
+)
